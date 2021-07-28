a6ac23186006230f0a0ea01c0b8603bbac4ae893
fatmacamalan <fatmacamalan28@gmail.com>
2021-07-28 23:10:48 +0000

    added info

--- key-pair/output.tf
+++ key-pair/output.tf
@@ -1,11 +1,9 @@
-data "aws_region" "current" {}
-
 output "region" {
-  value = data.aws_region.current.name
+  value = "us-east-2"
 }
 output "key_name" {
   value = aws_key_pair.ilearning-wordpress.key_name
 }
 output "key_id" {
-  value = aws_key_pair.ilearning-wordpress.key_pair_id
+  value = aws_key_pair.ilearning-wordpress.id
 }
