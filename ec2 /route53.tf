1b4bd8220890bf683e2a6b0df057945b82189222
fatmacamalan <fatmacamalan28@gmail.com>
2021-07-28 21:01:44 +0000

    added route

---
+++ ec2 /route53.tf
@@ -0,0 +1,8 @@
+resource "aws_route53_record" "www" {
+  zone_id = "Z00129912ZGS50OHITZ20"
+  name    = "wordpress.fatmacamalan.net"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.web.public_ip]
+
+}
