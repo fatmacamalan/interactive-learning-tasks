f7d1f66838a3929cf0e8c3eaf82d06561f2369e3
fatmacamalan <fatmacamalan28@gmail.com>
2021-07-28 18:48:31 +0000

    added route

--- route53/route53.tf
+++ route53/route53.tf
@@ -1,6 +1,6 @@
-resource "aws_route53_record" "WWW" {
+resource "aws_route53_record" "blog" {
   zone_id = "Z00129912ZGS50OHITZ20"
-  name    = "wordpress.fatmacamalan.net"
+  name    = "blog.fatmacamalan.net"
   type = "A"
   ttl = "300"
   records = ["127.0.0.1"]
