8e167fd3f5bd1b1c4789d7d55179dabb71b6f60b
fatmacamalan <fatmacamalan28@gmail.com>
2021-07-28 19:27:49 +0000

    added output

---
+++ route53/output.tf
@@ -0,0 +1,11 @@
+output "SomeOutput" {
+  value = <<EOF
+        Zone_id   ${aws_route53_record.blog.zone_id}
+        Name      ${aws_route53_record.blog.name}
+        
+    EOF
+}
+
+output "records" {
+  value = aws_route53_record.blog.records
+}
