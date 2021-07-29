988911477659df99631310f10b0c651550f19676
fatmacamalan <fatmacamalan28@gmail.com>
2021-07-29 14:57:20 +0000

    added info

--- ec2 /route53.tf
+++ ec2 /route53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "wordpress" {
   zone_id = "Z00129912ZGS50OHITZ20"
   name    = "wordpress.fatmacamalan.net"
-  type    = "A"
+  type    = "CNAME"
   ttl     = "300"
   records = [aws_instance.web.public_ip]
 
